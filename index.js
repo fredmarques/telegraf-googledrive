4e9f6494901c243a8cd49e9dcc472a6aeb208486
Frederico Marques <frederico.marques.22@gmail.com>
2016-11-06 21:54:34 -0200

    added support to set description and reply with file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const google = require('googleapis');
 const extend = require('xtend');
+const fs = require('fs');
 
 const drive = google.drive({
     version: 'v3'
@@ -19,22 +20,80 @@ const getFolder = (rootId, fields, auth, callback) => {
     }, (err, res) => callback(err, res, rootId));
 };
 
+const getReplyFile = (fileId, fileType, path, auth, callback) => {
+    const dest = fs.createWriteStream(`${path}/${fileId}`);
+    drive.files.get({
+        fileId
+        , auth
+        , alt: 'media'
+    })
+    .on('end', () => {
+        callback(fileType, `${path}/${fileId}`);
+    })
+    .on('error', err => {
+        console.log('Error during download', err);
+    })
+    .pipe(dest);
+};
+
+const getDescription = (fileId, path, auth, callback) => {
+    const dest = fs.createWriteStream(`${path}/${fileId}`);
+    return drive.files.get({
+        fileId
+        , auth
+        , alt: 'media'
+    })
+    .on('end', () => {
+        callback(`${path}/${fileId}`);
+    })
+    .on('error', err => {
+        console.log('Error during getDescription', err);
+    })
+    .pipe(dest);
+};
+
 /* callback to handle Google Drive files (or an error).
  * in this callback method we just add to ctx.state
  * all files listed from google drive.
  */
-const callback = (ctx, next) => (err, res, rootId) => {
+const foldersHandler = (ctx, next) => (err, res, rootId) => {
     if (err) {
         console.log(err);
         return next();
     }
-
+    const descriptionId = res.files.filter(f => f.name === 'README.md' && !f.trashed)[0].id;
+    const files = res.files.filter(f => !f.trashed);
     const nextState = extend(ctx.state,
         { folders: {
-            [rootId]: res.files
+            [rootId]: files
+            , descriptionId
         } });
     ctx.state = nextState; //eslint-disable-line
     return next();
+};
+
+const sendReply = (ctx, next) => (fileType, file) => {
+    if (fileType === 'document') {
+        ctx.replyWithDocument(file);
+    } else if (fileType === 'video') {
+        ctx.replyWithVideo(file);
+    }
+    next();
+};
+
+const setDescription = (ctx, next) => path => { //eslint-disable-line
+    return fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        const description = data || 'No descrition setted';
+        const nextState = extend(ctx.state.folders,
+            {
+                description
+            });
+        ctx.state.folders = nextState //eslint-disable-line
+        next();
+    });
 };
 
 /* Create a middleware for Telegraf
@@ -43,10 +102,24 @@ const callback = (ctx, next) => (err, res, rootId) => {
  * ctx and next are the parameters from Telegraf. For more refer to Telegraf docs
  * sessoin about middleware.
  */
-const createMiddleware = params => (ctx, next) => {
+const createGetFolderMiddleware = params => (ctx, next) => {
     const { fields, rootId, auth } = params;
-
-    return getFolder(rootId, fields, auth, callback(ctx, next));
+    return getFolder(rootId, fields, auth, foldersHandler(ctx, next));
 };
-module.exports = createMiddleware;
+
+const replyFileMiddleware = params => (ctx, next) => {
+    const { fileId, fileType, path } = params;
+    return getReplyFile(fileId, fileType, path, sendReply(ctx, next));
+};
+
+const setDescriptionMiddleware = params => (ctx, next) => {
+    const { path, auth } = params;
+    return getDescription(ctx.state.folders.descriptionId, path, auth, setDescription(ctx, next));
+};
+
+module.exports = {
+    getFolder: createGetFolderMiddleware
+    , replyFile: replyFileMiddleware
+    , setDescription: setDescriptionMiddleware
+};
 
